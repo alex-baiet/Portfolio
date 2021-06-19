ae21853589952f1fa1ba3866bd98850959336767
Alex BAIET <alex.baiet3@gmail.com>
2021-06-19 11:42:15 +0200

    Ajout compétences

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="fr" class="size300">
+<html lang="fr">
   <head>
     <title>Alex BAIET | Acceuil</title>
     <meta charset="utf-8">
@@ -77,36 +77,54 @@
 
         <div class="pres_part">
           <h3>Langages informatiques</h3>
-          <h4>Maitrisés</h4>
+          <h4><b>Langages logiciel</b></h4>
           <ul>
             <li>Python</li>
+            <li>Java</li>
             <li>C#</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Kotlin (développement mobile)</li>
+            <li>C++</li>
+            <li>C</li>
           </ul>
 
-          <h4>En cours d'apprentissage</h4>
+          <h4><b>Langages web</b></h4>
           <ul>
-            <li>Bash (Linux)</li>
-            <li>Powershell</li>
-            <li>DOS</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>Javascript</li>
           </ul>
-        </div>
-        
-        <div class="pres_part">
+          
           <h3>Langues</h3>
           <ul>
             <li>Anglais : B2</li>
           </ul>
 
+        </div>
+        
+        <div class="pres_part">
           <h3>Environnements</h3>
+          <h4><b>Systèmes</b></h4>
+          <ul>
+            <li>Windows</li>
+            <li>Linux</li>
+          </ul>
+
+          <h4><b>IDE / Editeur de code</b></h4>
           <ul>
             <li>Unity</li>
             <li>Visual Studio</li>
-            <li>Android Studio</li>
+            <li>Visual Studio Code</li>
+            <li>Eclipse</li>
+            <li>MySQL</li>
+            <li>PostgreSQL</li>
+          </ul>
+
+          <h4><b>Autres</b></h4>
+          <ul>
             <li>LibreOffice</li>
             <li>GitHub</li>
+            <li>Krita (Logiciel de dessin)</li>
           </ul>
         </div>
       </article>
